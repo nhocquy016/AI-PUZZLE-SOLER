587a1e8e1165a8f08501223b4304ae63d86a51e2
kurofake <duyduc1472006@gmail.com>
2026-06-01 12:58:55 +0700

    feat: Do Duy Duc - Genetic Algorithm, Differential Evolution, train script

--- evolution/optimizer.py
+++ evolution/optimizer.py
@@ -1,4 +1,5 @@
-# Author: Do Duy Duc - Genetic Algorithm & Differential Evolution f"""
+# Author: Do Duy Duc - Genetic Algorithm & Differential Evolution f
+"""
 evolution/optimizer.py
 ======================
 Tối ưu trọng số Neural Network bằng thuật toán tiến hóa.
